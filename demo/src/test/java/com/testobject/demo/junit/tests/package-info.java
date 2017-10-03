26322ae9c2d4d01be6db1b79da28d1c18e02d61a
gsaketh369 <32492944+gsaketh369@users.noreply.github.com>
2017-10-03 13:14:32 -0400

    Added new Project

---
+++ demo/src/test/java/com/testobject/demo/junit/tests/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Shopping App Test Suites Execution using JUnit
+ * 
+ * @author saketh (gsaketh369@gmail.com)
+ */
+package com.testobject.demo.junit.tests;
---
+++ demo/src/test/java/com/testobject/demo/testng/tests/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Shopping App Test Suites Execution using TestNG
+ * 
+ * @authorsaketh (gsaketh369@gmail.com)
+ */
+package com.testobject.demo.testng.tests;
